5c1de398202ce20fd702a345dbecc299bc134c5c
SamuelDaza21 <dazasamuel.1945@gmail.com>
2024-09-16 15:37:15 -0500

    version2
    recuadros de pagina principal redes sociales y contenido

--- OndaXplorer/codigo.js
+++ OndaXplorer/codigo.js
@@ -34,8 +34,41 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  document.addEventListener('click', showMainContent);
+  //document.addEventListener('click', showMainContent);
 });
 
+//Tarjetas del Inicio.
+function toggleCards() {
+  const wrapper = document.getElementById('cards-wrapper');
+  const content = document.getElementById('content');
+  const mainCard = document.getElementById('main-card');
+
+  const wrapperVisibility = window.getComputedStyle(wrapper).visibility;
+
+  if (wrapperVisibility === 'visible') {
+      // Ocultar tarjetas y contenido
+      wrapper.style.visibility = 'hidden';
+      wrapper.style.opacity = '0';  // Para hacer una transición suave
+      content.style.display = 'none';
+      mainCard.style.display = 'block';
+  } else {
+      // Mostrar tarjetas
+      wrapper.style.visibility = 'visible';
+      wrapper.style.opacity = '1';  // Asegurar que sea visible con opacidad
+      mainCard.style.display = 'none';
+  }
+}
 
 
+
+function showContent(cardId) {
+  const content = document.getElementById('content');
+  const contentCard = document.getElementById(`content-${cardId}`);
+
+  // Ocultar todas las tarjetas de contenido
+  document.querySelectorAll('.content-card').forEach(card => card.style.display = 'none');
+
+  // Mostrar la tarjeta de contenido seleccionada
+  contentCard.style.display = 'block';
+  content.style.display = 'flex';
+}
